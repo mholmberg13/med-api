21d229a61735165316243f28a0c6b324adf60b58
Matt Holmberg <mholmberg13@gmail.com>
2020-12-15 02:47:59 -0700

    Added create route to Order controller.

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const router = express.Router();
-const Users = require('../models/users.js');
+const Orders = require('../models/orders.js');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 const auth = require('../middleware/auth')
 const cors = require('cors');
 
 router.get("/orders", auth, async (req, res) => {
+    Orders.find();
+})
+
+router.post("/orders", auth, async (req, res) => {
+    
+    try { 
+        const newOrder = new Order({
+            orderer_name,
+            order_email,
+            product_amount,
+            street_address,
+            city,
+            state_region,
+            country
+
+        });
+
+        const savedOrder = await newOrder.save();
+        res.json(savedOrder);
+
+    } catch (err) {
+        res.status(500).json({error: err.message})
+    }
+})
+
+router.put("/orders", auth, async (req, res) => {
 
 })
 
+router.delete("/orders", auth, async (req, res) => {
+
+})
 
 
 
